32f6080235c1ad204db2faabbc3f09b8eb5cffbd
yafan37 <yfan37@hawk.iit.edu>
2024-03-22 15:25:10 +0800

    Initial commit

---
+++ lab3/Script/script.js
@@ -0,0 +1,27 @@
+const billTotalInput = document.getElementById("billTotal");
+const tipPercentageInput = document.getElementById("tipPercentage");
+const tipAmountInput = document.getElementById("tipAmount");
+const totalWithTipInput = document.getElementById("totalWithTip");
+const tipRangeInput = document.getElementById("tipRange");
+const errorDiv = document.getElementById("error");
+
+function tipValues() {
+    const billTotal = parseFloat(billTotalInput.value);
+    const tipPercentage = tipRangeInput.value;
+    
+    if (isNaN(billTotal)) {
+        errorDiv.textContent = "Please enter a valid number for Bill total.";
+        tipPercentageInput.value = "";
+        tipAmountInput.value = "";
+        totalWithTipInput.value = "";
+    } else {
+        errorDiv.textContent = "";
+        tipPercentageInput.value = tipPercentage + "%";
+        const tipAmount = (billTotal * tipPercentage) / 100;
+        tipAmountInput.value = "$" +  tipAmount.toFixed(2);
+        totalWithTipInput.value = "$" + (billTotal + tipAmount).toFixed(2);
+    }
+}
+
+billTotalInput.addEventListener("input", tipValues);
+tipRangeInput.addEventListener("input", tipValues);
